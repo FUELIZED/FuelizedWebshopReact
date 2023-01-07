d12ab33b3ae551638bef635b3b13c2692464b96d
Henry Vergote <henry.vergote@student.hogent.be>
2023-01-07 13:59:48 +0100

    nav + css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
     return (
-        <>
-            <h1 className="pb-4">⚠️404 Not Found⚠️</h1>
-            <a href="/" className="">Go back</a>
-        </>
+        <div className="grid place-items-center h-screen">
+            <div className="">
+                <h1 className="pb-4 text-white text-2xl">⚠️404 Not Found⚠️</h1>
+                <Link to="/" className="ml-auto place-self-center text-white hover:text-indigo-400 bold">← Go back</Link>
+            </div>
+        </div>
     )
 };
