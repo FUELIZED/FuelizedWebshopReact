74ac7f685f443c09c211f7692b87a22426994511
FUELIZED <122153083+FUELIZED@users.noreply.github.com>
2023-01-26 15:41:49 +0100

    Added Errer 404 : Page not found text

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ export const NotFound = () => {
     return (
         <div className="grid place-items-center h-screen">
             <div className="">
-                <h1 className="pb-4 text-white text-2xl">⚠️404 Not Found⚠️</h1>
+                <h1 className="pb-4 text-white text-2xl">⚠️Error 404 : Page Not Found⚠️</h1>
                 <Link to="/" className="ml-auto place-self-center text-white hover:text-indigo-400 bold">← Go back</Link>
             </div>
         </div>
