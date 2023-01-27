348e8871a50bbf9ea2b78d8c202a20a785cf4000
FUELIZED <122153083+FUELIZED@users.noreply.github.com>
2023-01-27 15:05:41 +0100

    Update music.jsx
    Texts + Link (no CSS yet)

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -8,6 +8,27 @@ export const Music = () => {
             <Nav active="music"/>
             <h1 className='pb-12 font-semibold text-6xl'>Music</h1>
             <h2>Priviously released music (embedded iframes from odesli.com) Also on top Spotify & Apple music embed. Chronological order bottom to top</h2>
+
+            (Spotify artist link)
+            https://open.spotify.com/artist/2xcgEqf4X5xcerwDFEcefI 
+            <hr></hr>
+            (Apple music artist link)
+            https://music.apple.com/us/artist/fuelized/1388443720
+            <hr></hr>
+            RELEASES 2022
+            <hr></hr>
+            <p>30.05.2022     
+            Fuelized & A to C
+            Stay
+            [Good4Nothing Records.]</p> 
+            Link: https://song.link/9x3wkspnzmqs8
+            <hr></hr>
+            <p>11.02.2022  
+            Damageman & Fuelized
+            Generation Z
+            [Good4Nothing Records.]</p> 
+            Link: https://song.link/2zcjg0bdfstss
+            <hr></hr>
             <div className='relative bottom-0'>
             <Footer />
             </div>
