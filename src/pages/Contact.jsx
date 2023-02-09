a59284cfea7749c161c289c1782116ceca6d6dc1
Pieter-Jan Couckuyt <fuelizeddnb@gmail.com>
2023-02-09 11:47:50 +0100

    Update Contact Form
    - NO CSS ADDED YET
    - FORM DOES NOT LAND YET AT contact@fuelized.com (need to be fixed...)
    
    - First name
    - Last name
    - Country of living
    - E-mail address
    - Date of birth
    - Phone number (incl. landcode)
    - Message field

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,64 +2,66 @@ import { Nav } from './components/Nav.jsx';
 import { Footer } from './components/Footer.jsx';
 import { BackToTop } from './components/BackToTop.jsx';
 
-
-
 export const Contact = () => {
     return (
         <>
             <Nav active="contact"/>
             <h1 className='pb-12 font-semibold text-6xl'>Contact</h1>
-            <div className='relative bottom-0'>
-                        <Footer />
-            </div>
         <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-
-  <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-
 <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
+
+<label>
+    First name:
+    <br></br><input type="text" name="name" />
+</label>
+
+<br></br>
+
+<label>
+    Last name:
+    <br></br><input type="text" name="name" />
+</label>
+
+<br></br>
+
+<label>
+    Country of living:
+    <br></br><input type="country" name="country" />
+</label>
+
+<br></br>
+
+<label>
+    E-Mail address:
+    <br></br><input type="text" name="name" />
+</label>
+
+<br></br>
+
+<label>
+    Date of birth:
+    <br></br><input type="date" name="date" />
+</label>
+
+<br></br>
+
+<label>
+    Phone number (Add your landcode):
+    <br></br><input type="phone" name="date" />
+</label>
+
+<br></br>
+
+<label>
+    Message:
+    <br></br><input type="message" name="message" />
+</label>
+</form>
 </form>
 
-<form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-
-<form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-
-
-<form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-</form>
+<div className='relative bottom-0'>
+<Footer />
+            </div>
             <BackToTop />  
         </>
         
