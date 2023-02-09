1f151fdd7d0607a1a7ec16f7f263aad33b294a73
Pieter-Jan Couckuyt <fuelizeddnb@gmail.com>
2023-02-09 11:26:18 +0100

    Test contatformings
    w

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,16 +2,64 @@ import { Nav } from './components/Nav.jsx';
 import { Footer } from './components/Footer.jsx';
 import { BackToTop } from './components/BackToTop.jsx';
 
+
+
 export const Contact = () => {
     return (
         <>
             <Nav active="contact"/>
             <h1 className='pb-12 font-semibold text-6xl'>Contact</h1>
-            <h2>This place needs a fully functional contact form arriving at contact@fuelized.com</h2>
-            <h3>Tabs: First name, last name, mail address + confirmation, country, subject of contact, phonenumber (including land code) message</h3>
             <div className='relative bottom-0'>
                         <Footer />
             </div>
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+
+  <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+</form>
             <BackToTop />  
         </>
         
