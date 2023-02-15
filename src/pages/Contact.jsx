01221ae5e196c945f3c143664c34fdb22bf5f348
Henry Vergote <henry.vergote@student.hogent.be>
2023-02-15 21:25:16 +0100

    contact styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,73 +1,52 @@
 import { Nav } from './components/Nav.jsx';
 import { Footer } from './components/Footer.jsx';
-import { BackToTop } from './components/BackToTop.jsx';
 
 export const Contact = () => {
+    const labelStyle = "text-white m-2"
+    const inputStyle = "bg-zinc-800 text-white rounded-lg p-1 px-2 w-96 m-2"
+    const requiredField = <div className='text-red-600 inline'>*</div>
+
     return (
         <>
-            <Nav active="contact"/>
-            <h1 className='pb-12 font-semibold text-6xl'>Contact</h1>
-        <form>
-<form>
+            <Nav active="contact" />
+            <h1 className='pb-4 font-semibold text-4xl'>Contact</h1>
+                <form className='[&>*]:m-2 grid place-items-center mb-20'>
+                    <table className='[&>*>*]:m-2 m-0 rounded-lg bg-zinc-900'>
+                        <tr>
+                            <td><label htmlFor="name" className={`${labelStyle} w-48`}>{requiredField} Full name</label></td>
+                            <td><input type="text" name="name" id='name' placeholder='Henry Vergote' required className={`${inputStyle}`}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="country" className={`${labelStyle}`}>Country of living</label></td>
+                            <td><input type="text" name="country" id='country' placeholder='Belgium' className={`${inputStyle}`}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="mail" className={`${labelStyle}`}>{requiredField} e-mail</label></td>
+                            <td><input type="email" name="mail" id="mail" placeholder='firstname.lastname@mail.com' className={`${inputStyle}`}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="date" className={`${labelStyle}`}>Date of birth</label></td>
+                            <td><input type="date" name="date" id="date" className={`${inputStyle}`}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="phone" className={`${labelStyle}`}>Phone number (add your landcode)</label></td>
+                            <td><input type="tel" name="phone" id="phone" placeholder='+32470123456' className={`${inputStyle}`}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="message" className={`${labelStyle}`}>{requiredField} Message</label></td>
+                            <td><textarea name="message" id="message" rows="5" placeholder='Your text here' required className={`${inputStyle} resize-none`}></textarea></td>                            
+                        </tr>
+                        <tr>
+                            <td colSpan={2} className="text-center"><button type="" className='bg-blue-600 hover:bg-blue-800 p-2 mb-2 rounded-lg '>Submit</button></td>
+                        </tr>
+                    
+                    </table>
+                </form>
 
-<label>
-    First name:
-    <br></br><input type="text" name="name" />
-</label>
-
-<br></br>
-
-<label>
-    Last name:
-    <br></br><input type="text" name="name" />
-</label>
-
-<br></br>
-
-<label>
-    Country of living:
-    <br></br><input type="country" name="country" />
-</label>
-
-<br></br>
-
-<label>
-    E-Mail address:
-    <br></br><input type="text" name="name" />
-</label>
-
-<br></br>
-
-<label>
-    Date of birth:
-    <br></br><input type="date" name="date" />
-</label>
-
-<br></br>
-
-<label>
-    Phone number (Add your landcode):
-    <br></br><input type="phone" name="date" />
-</label>
-
-<br></br>
-
-<label>
-    Message:
-    <br></br><input type="message" name="message" />
-</label>
-
-<br />
-
-<input type="submit" value="Send message now button..." />
-</form>
-</form>
-
-<div className='relative bottom-0'>
-<Footer />
+            <div className='fixed w-full bottom-0'>
+                <Footer />
             </div>
-            <BackToTop />  
         </>
-        
+
     )
 };
