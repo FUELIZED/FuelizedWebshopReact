09ca52992f3235f57b6cac345b3b8cecefd0323a
Henry Vergote <henry.vergote@student.hogent.be>
2023-02-22 09:47:36 +0100

    events update

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -7,17 +7,37 @@ export const Events = () => {
     return (
         <>
             <Nav active="events"/>
-            <h1 className='pb-12 font-semibold text-6xl'>Events</h1>
+            <h1 className='pb-12 font-semibold text-6xl text-center mt-6'>Upcoming events</h1>
             {/* <h2>An agenda with events chronologically announced with a counter till the event. + Google agenda connect?</h2> */}
-            <div className='w-4/5 mx-auto flex gap-2'>
+            <div className='w-4/5 mx-auto flex gap-2 flex-wrap justify-center'>
                 <Event 
-                    eventname="Test event"
-                    date="2021-10-10"
-                    time="20:00"
-                    location="https://goo.gl/maps/jhy9o2ULaamaoH6m6"
-                    locationName="Test location"
-                    // link="https://soundcloud.com/fuelizeddnb"
+                    eventname="Dropzone"
+                    date="01-04-2023"
+                    time="21:00"
+                    location="https://goo.gl/maps/qyBdmWzY5dyHQh6M8"
+                    locationName="Jeugdhuis Jester Ruiselede"
+                    image="/src/assets/dropzone.jpg"
+                    link="https://www.facebook.com/events/944968660258103"
                 />
+                <Event 
+                    eventname="Herres: enter the rave cave"
+                    date="26-06-2022"
+                    time="21:00"
+                    location="https://goo.gl/maps/JuBtWbChtwVfzaGf9"
+                    locationName="Barsol Kortrijk"
+                    image="/src/assets/herres.jpg"
+                    link="https://www.facebook.com/events/314207970868851"
+                />
+                <Event eventname="Test event" date="22-02-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 2" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 3" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 4" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 5" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 6" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 7" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 8" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 9" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
+                <Event eventname="Test event 10" date="10-10-2023" time="20:00" location="https://goo.gl/maps/jhy9o2ULaamaoH6m6" locationName="Test location" />
             </div>
             <div className='relative bottom-0'>
                 <Footer />
--- src/pages/components/Event.jsx
+++ src/pages/components/Event.jsx
@@ -1,17 +1,27 @@
-import { MdDateRange } from 'react-icons/md';
+import { MdDateRange, MdLocationPin } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
-export const Event = (arg) => {
-    return (
-        <div className="bg-zinc-700 w-full rounded-lg">
-            <div className="flex">
-                <div className="bg-zinc-50 w-80 aspect-square mr-4 rounded-l-lg"></div>
-                <div className="">
-                    <h1 className="text-5xl font-semibold my-2">{arg.eventname}</h1>
-                    <p className="text-sm flex place-items-center gap-2"><MdDateRange/> {arg.date}<span className="text-xs">{arg.time}</span></p>
-                    <a href={arg.location} target="_blank" className="text-sm">{arg.locationName}</a>
-                    {/* <Link to={arg.location} className="text-sm">{arg.locationName}</Link> */}
 
+export const Event = (arg) => {
+    const linkTo = () => {
+        if (arg.link) {
+            window.open(arg.link, '_blank');
+        }
+    }
+    // reverse date
+    let d = arg.date.split('-');
+    d = `${d[2]}-${d[1]}-${d[0]}`;
+    if (d < new Date().toISOString().slice(0, 10)) return null; // comment deze lijn om past events ook te laten zien
+    return (
+        <div className="bg-zinc-700 rounded-lg drop-shadow-xl hover:bg-zinc-600 transition">
+            <div className="flex flex-col">
+                <div className={`w-80 aspect-video ${arg.image ? "bg-none" : "bg-zinc-50"} rounded-t-lg`}><img src={arg.image} alt="event foto" className={`h-full w-full justify-center rounded-t-lg ${arg.link ? "cursor-pointer":""}`} onClick={linkTo}/></div>
+                <div className="w-80 p-3 pt-2 flex flex-col">
+                    <h1 className="text-3xl font-semibold mb-1">{arg.eventname}</h1>
+                    <p className="text-sm flex place-items-center gap-2"><MdDateRange/> {arg.date}<span className="text-xs">{arg.time}</span></p>
+                    {/* <a href="" className='text-sm my-1'>More info</a> */}
+                    <a href={arg.location} target="_blank" className="text-sm bg-blue-800 hover:bg-blue-700 hover:drop-shadow-lg py-1 rounded-full px-2 mt-2 w-max flex place-items-center gap-1"><MdLocationPin/> {arg.locationName}</a>
+                    {/* <Link to={arg.location} className="text-sm">{arg.locationName}</Link> */}
                 </div>
             </div>
         </div>
